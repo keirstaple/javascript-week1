25068682081d5dbcaf0ba0f53298b22a76f00dde
Keir Staple <keirstaple@comcast.net>
2016-03-20 23:41:34 -0700

    add javascript for retrieving github data using api

---
+++ js/githubUser.js
@@ -0,0 +1,11 @@
+var apiKey = require('./../.env').apiKey;
+
+var userRepos = function(githubUser) {
+  $.get('https://api.github.com/users/' + githubUser + '/repos?access_token=' + apiKey).then(function(userInfo) {
+    for (var i in userInfo) {
+      $('.githubRepos').append('<li><a href="' + userInfo[i].html_url + '">' + userInfo[i].name + '</li></a><p>' + userInfo[i].description + '</p>');
+    }
+  });
+}
+
+exports.userRepos = userRepos;
